4a39366f3fa62539df2202165256bf709ed0f8d7
qianyanxiu <471879661@qq.com>
2014-12-14 12:12:47 +0800

    qyx

--- src/com/pku/edu/BatchDownload.java
+++ src/com/pku/edu/BatchDownload.java
@@ -1,4 +1,4 @@
-package com.pku.edu;
+﻿package com.pku.edu;
 
 
 import java.io.BufferedInputStream;
@@ -35,46 +35,24 @@ import org.jsoup.select.Elements;
 
 public class BatchDownload {
 
-    /**
-     * ����˵��������ץȡ��ҳͼƬ���ص�����
-     */
     public static void main(String[] args) {
-        //�ٶ�ͼƬ
+       
         String fromUrl = "http://www.douban.com/search?cat=1025&q=%E6%A2%85%E8%A5%BF";
 
         StringBuffer pageContents = new StringBuffer();
-        //System.out.println("asdasdas");
+      
         try {
 
             URL startUrl = new URL(fromUrl);
             pageContents = downloadPage(startUrl);
-            //System.out.println(pageContents);
-            // if(pageContents.length()==0) System.out.println("asdasdas");
+          
             List<String> imgUrls = getImageUrls(pageContents);
-            //System.out.println(imgUrls.size());
-
-            //downloadImages(pageContents);
-            //if(pageContents.length()==0) System.out.println("asdasdas");
-            //System.out.println(pageContents);
+            
         } catch (Exception e) {
 
         }
 
 
-    }
-
-    public static StringBuffer downloadPage(URL httpUrl) throws MalformedURLException, IOException {
-        StringBuffer data = new StringBuffer();
-        String currentLine;
-        // ��������
-        BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(httpUrl), "GBK"));
-        // ��ȡ���
-        while ((currentLine = reader.readLine()) != null) {
-            data.append(currentLine);
-        }
-        reader.close();
-
-        return data;
     }
 
     public static File downloadFile(String httpUrl, String fileSavePath) throws MalformedURLException, IOException {
@@ -83,15 +61,15 @@ public class BatchDownload {
             file.createNewFile();
         }
         URL url = new URL(httpUrl);
-        // ��������
+       
         BufferedInputStream in = new BufferedInputStream(
                 getInputStream(url));
 
-        // �������
+       
         FileOutputStream out = new FileOutputStream(file);
 
         byte[] buff = new byte[1];
-        // ��ȡ���
+  
         while (in.read(buff) > 0) {
             out.write(buff);
         }
@@ -101,12 +79,33 @@ public class BatchDownload {
         return file;
     }
 
+    public static StringBuffer downloadPage(URL httpUrl) throws MalformedURLException, IOException {
+        StringBuffer data = new StringBuffer();
+        String currentLine;
+       
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStream(httpUrl), "GBK"));
+       
+        while ((currentLine = reader.readLine()) != null) {
+            data.append(currentLine);
+        }
+        reader.close();
+        return data;
+    }
+
+    private static InputStream getInputStream(URL httpUrl) throws IOException {
+  
+        URL url = httpUrl;
+        URLConnection uc = url.openConnection();
+        uc.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        return uc.getInputStream();
+    }
+
     private static void downloadImages(StringBuffer pageContents) throws MalformedURLException, IOException {
-        // ��ȡhtmlҳ��
+
         StringBuffer page = pageContents;
-        // ��ȡҳ���еĵ�ַ
+
         List<String> imgUrls = getImageUrls(page);
-        // ����ͼƬ�������ļ��б�
+
         List<File> fileList = new ArrayList<File>();
         String imgSaveDir = "E:";
         int i = 1;
@@ -118,29 +117,13 @@ public class BatchDownload {
             i++;
         }
     }
-
-    private static InputStream getInputStream(URL httpUrl) throws IOException {
-        // ��ҳUrl
-        URL url = httpUrl;
-        URLConnection uc = url.openConnection();
-        uc.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-        return uc.getInputStream();
-    }
-
-
     public static List<String> getImageUrls(StringBuffer html) {
         List<String> result = new ArrayList<String>();
-        // ���ַ����Ϊhtml�ĵ�
-        Document doc = Jsoup.parse(html.toString());
-        // ��ȡimg��ǩ
-        Elements es = doc.getElementsByTag("img");
-        //Element es = doc.getElementById("script");
-        //Elements ss = new Elements();
-        //ss.add(es);
-        //System.out.println(es.size());
-        //doc.getElementsByTag("img");
 
-        // ��ȡÿһ��img��ǩsrc�����ݣ�Ҳ����ͼƬ��ַ
+        Document doc = Jsoup.parse(html.toString());
+ 
+        Elements es = doc.getElementsByTag("img");
+
         for (Iterator<Element> i = es.iterator(); i.hasNext(); ) {
             Element e = i.next();
             String r = e.attr("src");
